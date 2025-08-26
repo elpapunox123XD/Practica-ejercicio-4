8e7704643e23ab49ce58af90a81ed6c7923dd3d6
UPN <UPN@ULNLAB1536>
2025-08-26 17:16:58 -0500

    Agregar archivos de proyecto.

---
+++ Practica ejercicio 1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        double radio, area;
+        Console.Write("Ingrese el radio del círculo: ");
+        radio =
+            double.Parse(Console.ReadLine());
+
+        area = Math.PI *
+     Math.Pow(radio, 2);
+
+        Console.WriteLine("El área del circulo es: " + area);
+        Console.ReadKey();
+    }
+}
